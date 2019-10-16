fe043c91d4178affb7ed92b430890da60edc8237
nicola pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2019-10-16 14:32:19 +0200

    Nicola: check whoiam

--- include/name_sorting/name_sorting.h
+++ include/name_sorting/name_sorting.h
@@ -5,49 +5,47 @@
 namespace name_sorting
 {
   /**
-   * \brief 
-   * permutationName, reorder vectors. 
+   * \brief
+   * permutationName, reorder vectors.
    * *order_names* names in the desired order
-   * *names* current order of names, 
+   * *names* current order of names,
    * *position, *velocity*, *effort* vectors to be reordered
    */
-  bool permutationName( const std::vector<std::string>& order_names, 
+  bool permutationName( const std::vector<std::string>& order_names,
                         std::vector<std::string>& names,
-                        std::vector<double>& position, 
-                        std::vector<double>& velocity, 
-                        std::vector<double>& effort, 
+                        std::vector<double>& position,
+                        std::vector<double>& velocity,
+                        std::vector<double>& effort,
                         const std::string whoaim
                         );
-  
+
   /**
-   * \brief 
-   * permutationName, reorder vectors. 
+   * \brief
+   * permutationName, reorder vectors.
    * *order_names* names in the desired order
-   * *names* current order of names, 
+   * *names* current order of names,
    * *position, *velocity* vectors to be reordered
    */
-  bool permutationName( const std::vector<std::string>& order_names, 
+  bool permutationName( const std::vector<std::string>& order_names,
                         std::vector<std::string>& names,
-                        std::vector<double>& position, 
+                        std::vector<double>& position,
                         std::vector<double>& velocity,
                         const std::string whoaim
                       );
   /**
-   * \brief 
-   * permutationName, reorder vectors. 
+   * \brief
+   * permutationName, reorder vectors.
    * *order_names* names in the desired order
-   * *names* current order of names, 
+   * *names* current order of names,
    * *position vector to be reordered
    */
-  bool permutationName( const std::vector<std::string>& order_names, 
+  bool permutationName( const std::vector<std::string>& order_names,
                         std::vector<std::string>& names,
                         std::vector<double>& position,
                         const std::string whoaim
                       );
-  
 
-  
 }
 
 
-#endif 
+#endif
