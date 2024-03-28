96a44ecbb2097230bb73259c9f12da471192e9a8
Manuel <cari>
2024-03-28 14:10:51 +0100

    remove decrecated

--- include/name_sorting/name_sorting.hpp
+++ include/name_sorting/name_sorting.hpp
@@ -10,20 +10,6 @@
 
 namespace name_sorting
 {
-/**
- * \brief
- * permutationName, reorder vectors.
- * *order_names* names in the desired order
- * *names* current order of names,
- * *position, *velocity*, *effort* vectors to be reordered
- */
-[[deprecated("Use permutationName with std::stringstream* report")]]
-bool permutationName( const std::vector<std::string>& order_names,
-                      std::vector<std::string>& names,
-                      std::vector<double>& position,
-                      std::vector<double>& velocity,
-                      std::vector<double>& effort,
-                      const std::string& whoami);
 
 
 /**
--- src/name_sorting/name_sorting.cpp
+++ src/name_sorting/name_sorting.cpp
@@ -80,69 +80,6 @@ bool permutationName(  const std::vector<std::string>& order_names,
   return true;
 }
 
-bool permutationName(  const std::vector<std::string>& order_names,
-                       std::vector<std::string>& names,
-                       std::vector<double>& position,
-                       std::vector<double>& velocity,
-                       std::vector<double>& effort,
-                       const std::string& whoami)
-{
-  if (names.size()<order_names.size())
-  {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "[ %s ]The vector of names to be sorted has size %zu, that is smaller than vector of the sorted names (%zu)", whoami.c_str(),names.size(),order_names.size());
-    return false;
-  }
-  if (names.size()!=position.size())
-  {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "[ %s ]Input Mismatch. The size of the vector of names is %zu, while position size is %zu", whoami.c_str(),names.size(),position.size());
-    return false;
-  }
-  if (names.size()!=velocity.size())
-  {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "[ %s ]Input Mismatch. The size of the vector of names is %zu, while velocity size is %zu", whoami.c_str(),names.size(),velocity.size());
-    return false;
-  }
-  if (names.size()!=effort.size())
-  {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "[ %s ]Inut Mismatch. The size of the vecotr of the names is %zu, while effort size is %zu", whoami.c_str(),names.size(),effort.size());
-    return false;
-  }
-
-
-
-  for (unsigned int iOrder=0;iOrder<order_names.size();iOrder++)
-  {
-    if (names.at(iOrder).compare(order_names.at(iOrder)))
-    {
-      for (unsigned int iNames=iOrder+1;iNames<names.size();iNames++)
-      {
-        if (!order_names.at(iOrder).compare(names.at(iNames)))
-        {
-          std::iter_swap(names.begin()+iOrder,    names.begin()+iNames);
-          std::iter_swap(position.begin()+iOrder, position.begin()+iNames);
-          std::iter_swap(velocity.begin()+iOrder, velocity.begin()+iNames);
-          std::iter_swap(effort.begin()+iOrder,   effort.begin()+iNames);
-          break;
-        }
-        if (iNames==(names.size()-1))
-        {
-          std::stringstream ss;
-          RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "[ %s ]The Joint '%s' that is in the vector of the sorted names, is missing in the vector to be sorted.", whoami.c_str(),order_names.at(iOrder).c_str());
-          ss << "Sorted Names: <";
-          for( size_t i=0;i<order_names.size();i++)
-            ss << order_names.at(i) <<",";
-          ss << "> vs Names to be ordered: <";
-          for( size_t i=0;i<names.size();i++)
-            ss << names.at(i) <<",";
-          ss <<">";
-          RCLCPP_ERROR_STREAM(rclcpp::get_logger("rclcpp"),  ss.str() );
-          return false;
-        }
-      }
-    }
-  }
-  return true;
-}
 
 
 
